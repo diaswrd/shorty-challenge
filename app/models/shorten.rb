75820abfd9edd380d1bcf53e3a70bfb4f40a29bc
William Dias <diasforsaken@gmail.com>
2015-05-15 15:10:55 -0300

    Check url protocol and complete if necessary.

--- app/models/shorten.rb
+++ app/models/shorten.rb
@@ -1,20 +1,28 @@
 class Shorten < ActiveRecord::Base
 
+    before_validation :check_url_protocol
+
     after_initialize :set_defaults, unless: :persisted?
 
-    validates :url, presence: true, url: { no_local: true }
+    validates :url, presence: true, url: true
 
     validates :shortcode,
         uniqueness: true,
         allow_blank: true,
         format: {
             with: /\A[0-9a-zA-Z_]{4,}\z/,
-            message: "The shortcode should contain only alphanumeric characters (a - z, A - Z, 0 - 9)."
+            message: 'The shortcode should contain only alphanumeric characters (a - z, A - Z, 0 - 9).'
         },
         length: {
             minimum: 4,
-            message: "The shortcode should be at least 4 characters long."
+            message: 'The shortcode should be at least 4 characters long.'
         }
+
+    def check_url_protocol
+        unless self.url[/\Ahttp:\/\//] or self.url[/\Ahttps:\/\//]
+            self.url = "http://#{self.url}"
+        end
+    end
 
     def set_defaults
         self.last_seen_date ||= Time.now
