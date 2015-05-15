43cdb444810a07d622dcaedf4cf58585d1f5c7e8
William Dias <diasforsaken@gmail.com>
2015-05-15 16:29:02 -0300

    Add a shortcode generator when it's not already set.

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,10 +2,17 @@ FactoryGirl.define do
 
     factory :shorten do
         url 'google.com'
-    end
+        shortcode 'googl'
 
-    factory :invalid_url_shorten, parent: :shorten do
-        url ''
+        factory :invalid_url_shorten do
+            url ''
+            shortcode 'empty'
+        end
+
+        factory :no_shortcode_shorten do
+            url 'google.com'
+            shortcode nil
+        end
     end
 
 end
--- spec/models/shorten_spec.rb
+++ spec/models/shorten_spec.rb
@@ -11,4 +11,18 @@ RSpec.describe Shorten, type: :model do
         expect(shorten).to be_invalid
     end
 
+    it 'should auto-generate a shortcode if none is requested' do
+        shorten = FactoryGirl.create(:no_shortcode_shorten)
+
+        expect(shorten).to be_valid
+
+        expect(shorten.shortcode).to match(/\A[0-9a-zA-Z_]{6}\z/)
+    end
+
+    it 'should contain a redirect counter' do
+        shorten = FactoryGirl.create(:shorten)
+
+        expect(shorten.redirect_count).to eql(0)
+    end
+
 end
