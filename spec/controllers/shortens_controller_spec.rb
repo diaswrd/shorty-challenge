045b8f41b82853baa8a120ddf50e26194d24cd82
William Dias <diasforsaken@gmail.com>
2015-05-18 01:54:18 -0300

    Add spec for shorten controller.

---
+++ spec/controllers/shortens_controller_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe ShortensController, type: :controller do
+
+    it 'should shorten a url' do
+        post :create, { format: 'json', shorten: { url: 'http://example.com' } }
+
+        data = JSON.parse response.body
+
+        expect(response.header['Content-Type']).to include('application/json')
+        expect(data['shortcode'].length).to be > 0
+    end
+
+    it 'should shorten a url with a preferencial short code' do
+        post :create, { format: 'json', shorten: { url: 'http://example.com', shortcode: 'example' } }
+
+        data = JSON.parse response.body
+
+        expect(response.header['Content-Type']).to include('application/json')
+        expect(data['shortcode']).to eql('example')
+    end
+
+    it 'should return 409 if a shortcode is already taken' do
+        post :create, { format: 'json', shorten: { url: 'http://example.com', shortcode: 'example' } }
+
+        expect(response.status).to be(409)
+    end
+
+    it 'should return 422 if the preferencial shortcode contains not alphanumeric characters' do
+        post :create, { format: 'json', shorten: { url: 'http://example.com', shortcode: '^aa~a' } }
+
+        expect(response.status).to be(422)
+    end
+
+    it 'should return 422 if the preferencial shortcode isn’t at least 4 characters long.' do
+        post :create, { format: 'json', shorten: { url: 'http://example.com', shortcode: 'aaa' } }
+
+        expect(response.status).to be(422)
+    end
+
+    it 'should redirect to its respective url' do
+        post :create, { format: 'json', shorten: { url: 'http://example.com', shortcode: 'example' } }
+        get '/example'
+
+        expect(response.status).to be(302)
+    end
+
+end
