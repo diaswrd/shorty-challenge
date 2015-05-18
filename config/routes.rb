9b059e4f27052af51cf9bbd91012c91e6cc394a5
William Dias <diasforsaken@gmail.com>
2015-05-18 02:25:19 -0300

    Routes for shortens controller.

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,15 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'shortens#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+
+  get 'shortens' => 'shortens#index'
+  get ':shortcode' => 'shortens#redirect'
+
+  post 'shortens' => 'shortens#create'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
